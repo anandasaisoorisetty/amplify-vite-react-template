f0d545d3012ce62234db5040d4803d14066434a6
anandasaisoorisetty <107982965+anandasaisoorisetty@users.noreply.github.com>
2024-06-07 17:02:36 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
 
 const client = generateClient<Schema>();
 
@@ -23,6 +25,9 @@ function App() {
 
   
   return (
+        
+    <Authenticator>
+      {({ signOut }) => (
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
@@ -40,7 +45,12 @@ function App() {
           Review next step of this tutorial.
         </a>
       </div>
+          
+          <button onClick={signOut}>Sign out</button>
     </main>
+              
+      )}
+    </Authenticator>    
   );
 }
 
